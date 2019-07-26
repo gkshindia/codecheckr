1268d8c5208ceb4efdf6d3750336507f9156ddb0
gkshindia <gkshindia@gmail.com>
2019-07-26 09:09:43 +0530

    Attempt to fix the bug

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,23 +1,22 @@
-import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
+
+# instantiate the app
 app = Flask(__name__)
 
 api = Api(app)
 
 # set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+app.config.from_object('project.config.DevelopmentConfig')  # new
 
 
 class UsersPing(Resource):
-
     def get(self):
         return {
-            "status": "success",
-            "message": "ping pong"
-        }
+        'status': 'success',
+        'message': 'pong!'
+    }
 
 
 api.add_resource(UsersPing, '/users/ping')
