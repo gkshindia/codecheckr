c091773e62fcc9582b59b71b346ec81dc512fb00
gkshindia <gkshindia@gmail.com>
2019-08-13 20:32:23 +0530

    removing the watch mode

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ docker-compose exec users python manage.py test
 inspect $? users
 docker-compose exec users flake8 project
 inspect $? users-lint
-docker-compose exec client npm test -- --coverage
+docker-compose exec client npm test -- --watch
 inspect $? client
 docker-compose down
 
