7e86f212ddbceb672344e47739342ac95c7c8ba7
gkshindia <gkshindia@gmail.com>
2019-08-13 12:02:23 +0530

    Cypress Test Setup

---
+++ test.sh
@@ -0,0 +1,35 @@
+fails=""
+
+inspect() {
+  if [ $1 -ne 0 ]; then
+    fails="${fails} $2"
+  fi
+}
+
+# run unit and integration tests
+docker-compose up -d --build
+docker-compose exec users python manage.py test
+inspect $? users
+docker-compose exec users flake8 project
+inspect $? users-lint
+docker-compose exec client npm test -- --coverage
+inspect $? client
+docker-compose down
+
+# new
+# run e2e tests
+docker-machine env codecheckr-prod
+docker-compose -f docker-compose-prod.yml up -d --build
+docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
+./node_modules/.bin/cypress run --config baseUrl=http://54.196.163.157
+inspect $? e2e
+docker-compose -f docker-compose-prod.yml down
+
+# return proper code
+if [ -n "${fails}" ]; then
+  echo "Tests failed: ${fails}"
+  exit 1
+else
+  echo "Tests passed!"
+  exit 0
+fi
